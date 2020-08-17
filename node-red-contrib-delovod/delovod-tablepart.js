85a02f47d39644a9fa80a37f6e02edfa84912b19
max_tricky_lake <mdovhopo@student.unit.ua>
2020-08-17 16:46:41 +0300

    fix header formatting in delovod create item

--- node-red-contrib-delovod/delovod-tablepart.js
+++ node-red-contrib-delovod/delovod-tablepart.js
@@ -14,34 +14,23 @@ module.exports = function (RED) {
       if (!config.docType) return makeError(`docType is required`);
       if (!node.config) return makeError(`Config is required`);
 
-      const payload = msg.payload || {};
       try {
         const tableParts = Object.entries(JSON.parse(config.tableParts))
           .reduce((acc, [tablePart, tablePartValue]) => {
-            const resolveField = (val, baseObj) => {
-              const [type, value] = global.utils.parseTypedInput(val);
-              if (type === 'const') return value;
-              return global.utils.resolvePath(baseObj, value);
-            };
-
             const resolveFields = (baseObj) => {
               return Object.entries(tablePartValue)
-                .map(([key, value]) => [key, resolveField(value, baseObj)])
+                .map(([key, value]) => [key, global.utils.getTypedFieldValue(value, baseObj)])
                 .reduce((acc, item) => {
                   acc[item[0]] = item[1];
                   return acc;
                 }, {})
             }
-
-            acc[tablePart] = Array.isArray(payload)
-              ? payload.map(resolveFields)
-              : resolveFields(payload);
-            return acc
+            acc[tablePart] = resolveFields(msg);
+            return acc;
           }, {});
         msg.payload = {tableParts};
         node.send([null, msg]);
       } catch (e) {
-
         msg.payload = {error: e.message};
         node.send([msg, null]);
       }
