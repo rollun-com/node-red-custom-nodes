1440150d0381e57d96d79a249fa4c0d4e69bed54
max_tricky_lake <mdovhopo@student.unit.ua>
2020-09-02 12:19:58 +0300

    add MWS node

--- node-red-contrib-mws/mws-config.js
+++ node-red-contrib-mws/mws-config.js
@@ -6,6 +6,8 @@ module.exports = function (RED) {
     this.accessKeyId = config.accessKeyId
     this.secretAccessKey = config.secretAccessKey
     this.merchantId = config.merchantId
+    this.MWSAuthToken = config.MWSAuthToken;
+    this.marketplaceId = config.marketplaceId;
     this.host = config.host || "mws.amazonservices.com"
 
     this.client = new MwsApi();
--- node-red-contrib-mws/mws.js
+++ node-red-contrib-mws/mws.js
@@ -1,20 +1,55 @@
+const _ = require('lodash');
+
 module.exports = function (RED) {
   function MWSNode(config) {
+
     RED.nodes.createNode(this, config)
     this.action = config.action
     this.config = RED.nodes.getNode(config.config)
     const mws = this.config.client
     const node = this
     node.on('input', function (msg) {
-      node.send(msg);
-      // mws.client.finances.search({
-      //   'Version': '2015-05-01',
-      //   'Action': 'ListFinancialEventGroups',
-      //   'SellerId': 'SELLER_ID',
-      //   'MWSAuthToken': 'MWS_AUTH_TOKEN',
-      //   'FinancialEventGroupStartedAfter': new Date(2016, 11, 24)
-      // }).then(r => console.log('response', r))
-      //   .catch(err => console.log('error ', err))
+
+      const category = _.camelCase(config.category);
+      const api = mws[category];
+
+      if (!api) {
+        msg.payload = {error: `Unknown category: ${config.category} (${category})`}
+        return node.send([msg]);
+      }
+
+      const payload = global.utils.getTypedFieldValue(config.actionPayload, msg) || {};
+      if (typeof payload !== "object") {
+        msg.payload = {error: `Action payload must be an object`}
+        return node.send([msg]);
+      }
+
+      const version = global.utils.getTypedFieldValue(config.version, msg);
+      const action = global.utils.getTypedFieldValue(config.action, msg);
+
+      console.log(config, version, action);
+
+      api.search({
+        'Version': version,
+        'Action': config.action,
+        'SellerId': this.merchantId,
+        'MWSAuthToken': this.MWSAuthToken,
+        ...payload
+      }).then(r => {
+        console.log('response', r)
+        msg.payload = r;
+        node.send([null, msg]);
+      })
+        .catch(err => {
+          console.log('error ', err)
+          msg.payload = {
+            error: Object.entries(err).reduce((acc, [key, val]) => {
+              acc[key] = val;
+              return acc;
+            }, {})
+          };
+          node.send([msg, null]);
+        })
     });
   }
 
