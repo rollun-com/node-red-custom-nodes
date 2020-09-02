b899c09ea4ba5a3afcdb4068cdbb91b2794dabe8
max_tricky_lake <mdovhopo@student.unit.ua>
2020-09-02 14:40:21 +0300

    update mws

--- node-red-contrib-mws/mws.js
+++ node-red-contrib-mws/mws.js
@@ -15,33 +15,36 @@ module.exports = function (RED) {
 
       if (!api) {
         msg.payload = {error: `Unknown category: ${config.category} (${category})`}
-        return node.send([msg]);
+        return node.send([msg, null]);
       }
 
       const payload = global.utils.getTypedFieldValue(config.actionPayload, msg) || {};
       if (typeof payload !== "object") {
         msg.payload = {error: `Action payload must be an object`}
-        return node.send([msg]);
+        return node.send([msg, null]);
       }
 
       const version = global.utils.getTypedFieldValue(config.version, msg);
-      const action = global.utils.getTypedFieldValue(config.action, msg);
 
-      console.log(config, version, action);
 
-      api.search({
+      if (!api[config.actionType]) {
+        msg.payload = {
+          error: `Can't find requester with Action type '${config.actionType}' on Category '${category}' and action '${config.action}', try another type instead.`
+        }
+        return node.send([msg, null]);
+      }
+
+      api[config.actionType]({
         'Version': version,
         'Action': config.action,
         'SellerId': this.merchantId,
         'MWSAuthToken': this.MWSAuthToken,
         ...payload
-      }).then(r => {
-        console.log('response', r)
-        msg.payload = r;
+      }).then(res => {
+        msg.payload = res;
         node.send([null, msg]);
       })
         .catch(err => {
-          console.log('error ', err)
           msg.payload = {
             error: Object.entries(err).reduce((acc, [key, val]) => {
               acc[key] = val;
