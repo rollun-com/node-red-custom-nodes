60cc54908a1e5ceb4bfc69305ffd2a51250cf7a3
max-rollun-dev <max.dovhopoliy.dev@gmail.com>
2020-12-01 13:37:41 +0200

    add break

--- node-red-contrib-common-utils/array-map.js
+++ node-red-contrib-common-utils/array-map.js
@@ -1,257 +1,228 @@
+const {ArrayMapState} = require("./array-map-state");
 const {getTypedFieldValue} = require('../node-red-contrib-common-utils/1-global-utils')
+const _ = require('lodash');
 
 module.exports = function (RED) {
+    const state = new ArrayMapState(RED);
 
-  const _ = require('lodash');
+    function ArrayMapStart({isSync = false} = {}) {
+        return function (config) {
+            RED.nodes.createNode(this, config);
+            const node = this;
+            this.config = RED.nodes.getNode(config.config);
 
-  // Module to work with array-map-state
-  const {
-    addToResult, isFinished, getResult, clearResult
-  } = (() => {
-    // map _msgif -> array of results
-    // DO NOT MODIFY DIRECTLY
-    let arrayMapResults = {};
+            node.on('input', function (msg) {
 
-    const addToResult = ({payload, _msgid, type, key}) => {
-      if (!arrayMapResults[_msgid]) {
-        arrayMapResults[_msgid] = type === 'array' ? [] : {};
-      }
-      if (type === 'array') {
-        arrayMapResults[_msgid].push(payload);
-      } else {
-        if (key === null || key === undefined || key === '') {
-          key = RED.util.generateId()
+                const makeError = (node, text) => {
+                    msg.payload = {error: text};
+                    node.send([null, msg])
+                };
+
+                const interval = +config.interval;
+
+                if (!config.arrayField) return makeError(node, `arrayField is required!`);
+                if (isNaN(interval) || interval < 0) return makeError(node, `interval is required, and must be a number > 0!`);
+
+
+                (async () => {
+                    let iterable = getTypedFieldValue(msg, config.arrayField);
+
+                    if (typeof iterable !== 'object') {
+                        throw new Error('Data in arrayField must be either object or array!');
+                    }
+
+                    const type = Array.isArray(iterable) ? 'array' : 'object';
+
+                    if (type === 'object') {
+                        iterable = Object.entries(iterable);
+                    }
+
+                    if (iterable.length === 0) {
+                        return node.send([null, null, msg]);
+                    }
+
+                    const {req, res} = msg;
+
+                    if (req) delete msg.req;
+                    if (res) delete msg.res;
+
+                    const msgid = RED.util.generateId();
+                    let canIterate = true;
+                    let i = 0;
+                    let len = iterable.length;
+                    while (canIterate) {
+                        let key, value, index;
+                        if (type === 'array') {
+                            index = i;
+                            value = iterable[i];
+                        }
+
+                        if (type === 'object') {
+                            [key, value] = iterable[i];
+                        }
+
+                        const msgCopy = {
+                            _msgid: msgid,
+                            payload: _.cloneDeep(value),
+                            type,
+                            ...(index !== undefined && {index: i}),
+                            ...(key !== undefined && {key}),
+                            totalItemsAmount: len,
+                            topic: `Element #${i} of ${type}`,
+                            originalMsg: msg,
+                            req: req,
+                            res: res
+                        };
+
+                        node.send(msgCopy);
+
+                        if (isSync) {
+                            // store 'resolve' func for promise, to call it from ArrayMapEnd, to 'resume' execution
+                            // therefore creating 'sync' iteration effect
+                            await (new Promise(resolve => {
+                                arrayStartPromiseResolvers[msgCopy._msgid] = resolve;
+                                state.addBreakFn(msgCopy, () => {
+                                    canIterate = false;
+                                    resolve();
+                                })
+                            }))
+                        }
+
+                        await (new Promise(resolve => setTimeout(() => resolve(), +config.interval)));
+                        i++;
+                        if (i >= len) {
+                            canIterate = false;
+                        }
+                    }
+                })()
+                    .catch(err => {
+                        console.log(err);
+                        msg._isArrayMapError = true;
+                        msg.error = err.message;
+                        node.send(msg)
+                    })
+            });
+
         }
-        arrayMapResults[_msgid][key] = payload;
-      }
     }
 
-    const isFinished = ({_msgid, totalItemsAmount, type}) => {
-      if (type === 'array') {
-        return arrayMapResults[_msgid].length === totalItemsAmount
-      }
-      return Object.keys(arrayMapResults[_msgid]).length === totalItemsAmount;
-    }
+    RED.nodes.registerType("array-map-start", ArrayMapStart());
+    RED.nodes.registerType("array-map-start-sync", ArrayMapStart({isSync: true}));
 
-    const getResult = ({_msgid, type}, filterEmpty = false) => {
-      const toDel = (val) => val !== null && val !== undefined;
-      if (type === 'array') {
-        const arr = arrayMapResults[_msgid] || [];
-        return filterEmpty
-          ? arr.filter(toDel)
-          : arr
-      }
-      if (type === 'object') {
-        const obj = arrayMapResults[_msgid] || {};
-        return filterEmpty
-          ? Object.entries(obj)
-            .filter(([, val]) => toDel(val))
-            .reduce((acc, [key, value]) => {
-              acc[key] = value;
-              return acc;
-            }, {})
-          : obj
-      }
-    }
+    let arrayStartPromiseResolvers = {};
 
-    const clearResult = ({_msgid}) => {
-      if (arrayMapResults[_msgid]) {
-        delete arrayMapResults[_msgid];
-      }
-    }
+    function ArrayMapEnd({isSync = false} = {}) {
+        return function (config) {
+            RED.nodes.createNode(this, config);
+            const node = this;
+            this.config = RED.nodes.getNode(config.config);
 
-    return {
-      addToResult, isFinished, getResult, clearResult
-    }
-  })()
+            let timeouted = false;
+            let timeout;
+            const filterEmpty = !!config.filterEmpty;
+            const [, resultField] = (config.resultField || 'msg|payload').split('|');
 
-  function ArrayMapStart({isSync = false} = {}) {
-    return function (config) {
-      RED.nodes.createNode(this, config);
-      const node = this;
-      this.config = RED.nodes.getNode(config.config);
-
-      node.on('input', function (msg) {
-
-        const makeError = (node, text) => {
-          msg.payload = {error: text};
-          node.send([null, msg])
-        };
-
-        const interval = +config.interval;
-
-        if (!config.arrayField) return makeError(node, `arrayField is required!`);
-        if (isNaN(interval) || interval < 0) return makeError(node, `interval is required, and must be a number > 0!`);
-
-
-        (async () => {
-          let iterable = getTypedFieldValue(msg, config.arrayField);
-
-          if (typeof iterable !== 'object') {
-            throw new Error('Data in arrayField must be either object or array!');
-          }
-
-          const type = Array.isArray(iterable) ? 'array' : 'object';
-
-          if (type === 'object') {
-            iterable = Object.entries(iterable);
-          }
-
-          if (iterable.length === 0) {
-            return node.send([null, null, msg]);
-          }
-
-          const {req, res} = msg;
-
-          if (req) delete msg.req;
-          if (res) delete msg.res;
-
-          const msgid = RED.util.generateId();
-          for (let i = 0, len = iterable.length; i < len; i++) {
-            let key, value, index;
-            if (type === 'array') {
-              index = i;
-              value = iterable[i];
-            }
-
-            if (type === 'object') {
-              [key, value] = iterable[i];
-            }
-
-            const msgCopy = {
-              _msgid: msgid,
-              payload: _.cloneDeep(value),
-              type,
-              ...(index !== undefined && {index: i}),
-              ...(key !== undefined && {key}),
-              totalItemsAmount: len,
-              topic: `Element #${i} of ${type}`,
-              originalMsg: msg,
-              req: req,
-              res: res
+            const callArrayStartResolve = ({_msgid}) => {
+                arrayStartPromiseResolvers[_msgid] && arrayStartPromiseResolvers[_msgid]();
             };
 
-            node.send(msgCopy);
+            const clearArrayStartResolve = ({_msgid}) => {
+                if (arrayStartPromiseResolvers[_msgid]) {
+                    delete arrayStartPromiseResolvers[_msgid]
+                }
+            };
 
-            if (isSync) {
-              // store 'resolve' func for promise, to call it from ArrayMapEnd, to 'resume' execution
-              // therefore creating 'sync' iteration effect
-              await (new Promise(resolve => {
-                arrayStartPromiseResolvers[msgCopy._msgid] = resolve;
-              }))
-            }
+            node.on('input', function (msg) {
 
-            await (new Promise(resolve => setTimeout(() => resolve(), +config.interval)));
-          }
+                if (msg.__arrayMapBreakFlag) {
+                    const finalMsg = {
+                        ...(msg.originalMsg || {}),
+                        [resultField]: msg.__breakPayload
+                            ? msg.__breakPayload
+                            : state.getResult(msg, filterEmpty),
+                        req: msg.req,
+                        res: msg.res
+                    };
+                    state.clearResult(msg);
+                    state.getBreakFn(msg)();
+                    state.clearBreakFn(msg);
+                    isSync && clearArrayStartResolve(msg);
+                    timeout && clearTimeout(timeout);
+                    return node.send([null, finalMsg]);
+                }
 
-        })()
-          .catch(err => {
-            console.log(err);
-            msg._isArrayMapError = true;
-            msg.error = err.message;
-            node.send(msg)
-          })
-      });
+                !timeout && config.timeout > 0 && (timeout = setTimeout(() => {
+                    if (msg.originalMsg) {
+                        msg = msg.originalMsg;
+                    }
+                    state.clearResult(msg);
+                    msg.payload = {error: `Did not receive all items from array-map-start after ${config.timeout}ms`};
+                    node.send(msg);
+                    timeouted = true;
+                }, config.timeout));
 
+                if (timeouted) return;
+
+                if (msg._isArrayMapError === true || msg.totalItemsAmount === undefined) {
+                    const orgError = msg.error || 'Unknown error';
+                    state.clearResult(msg);
+                    isSync && clearArrayStartResolve(msg);
+                    if (msg.originalMsg) {
+                        const req = msg.req;
+                        const res = msg.res;
+                        msg = msg.originalMsg;
+                        msg.res = res;
+                        msg.req = req;
+                    }
+                    if (msg._isArrayMapError) {
+                        msg.payload = {
+                            error: orgError
+                        }
+                    } else {
+                        msg.payload = {
+                            error: 'It seems like, you accidentally deleted totalItemsAmount from message, do not do it please.'
+                        }
+                    }
+                    node.send(msg);
+                }
+
+                state.addToResult(msg);
+                isSync && callArrayStartResolve(msg);
+                if (state.isFinished(msg)) {
+                    try {
+                        const finalMsg = {
+                            ...(msg.originalMsg || {}),
+                            [resultField]: state.getResult(msg, filterEmpty),
+                            req: msg.req,
+                            res: msg.res
+                        };
+                        state.clearResult(msg);
+                        isSync && clearArrayStartResolve(msg);
+                        node.send(finalMsg);
+                        clearTimeout(timeout);
+                    } catch (e) {
+                        console.log('cannot send result msg after map', e);
+                    }
+                }
+            });
+        }
     }
-  }
 
-  RED.nodes.registerType("array-map-start", ArrayMapStart());
-  RED.nodes.registerType("array-map-start-sync", ArrayMapStart({isSync: true}));
+    function ArrayBreak() {
+        return function (config) {
+            RED.nodes.createNode(this, config);
+            const node = this;
+            this.config = RED.nodes.getNode(config.config);
 
-  let arrayStartPromiseResolvers = {};
-
-  function ArrayMapEnd({isSync = false} = {}) {
-    return function (config) {
-      RED.nodes.createNode(this, config);
-      const node = this;
-      this.config = RED.nodes.getNode(config.config);
-
-      let timeouted = false;
-      let timeout
-      const filterEmpty = !!config.filterEmpty;
-      const [, resultField] = (config.resultField || 'msg|payload').split('|');
-
-      const callArrayStartResolve = ({_msgid}) => {
-        arrayStartPromiseResolvers[_msgid] && arrayStartPromiseResolvers[_msgid]();
-      }
-
-      const clearArrayStartResolve = ({_msgid}) => {
-        if (arrayStartPromiseResolvers[_msgid]) {
-          delete arrayStartPromiseResolvers[_msgid]
+            node.on('input', msg => {
+                msg.__arrayMapBreakFlag = true;
+                msg.__breakPayload = msg.payload;
+                node.send(msg);
+            })
         }
-      }
-
-      node.on('input', function (msg) {
-
-        !timeout && config.timeout > 0 && (timeout = setTimeout(() => {
-          if (msg.originalMsg) {
-            msg = msg.originalMsg;
-          }
-          clearResult(msg);
-          msg.payload = {error: `Did not receive all items from array-map-start after ${config.timeout}ms`};
-          node.send(msg);
-          timeouted = true;
-        }, config.timeout));
-
-        if (timeouted) return;
-
-        if (msg._isArrayMapError === true || msg.totalItemsAmount === undefined) {
-          const orgError = msg.error || 'Unknown error';
-          clearResult(msg);
-          isSync && clearArrayStartResolve(msg)
-          if (msg.originalMsg) {
-            const req = msg.req;
-            const res = msg.res;
-            msg = msg.originalMsg;
-            msg.res = res;
-            msg.req = req;
-          }
-          if (msg._isArrayMapError) {
-            msg.payload = {
-              error: orgError
-            }
-          } else {
-            msg.payload = {
-              error: 'It seems like, you accidentally deleted totalItemsAmount from message, do not do it please.'
-            }
-          }
-          node.send(msg);
-        }
-
-        addToResult(msg);
-        isSync && callArrayStartResolve(msg);
-        if (isFinished(msg)) {
-          try {
-            const finalMsg = {
-              ...(msg.originalMsg || {}),
-              [resultField]: getResult(msg, filterEmpty),
-              req: msg.req,
-              res: msg.res
-            }
-            clearResult(msg);
-            isSync && clearArrayStartResolve(msg)
-            node.send(finalMsg);
-            clearTimeout(timeout);
-          } catch (e) {
-            console.log('cannot send result msg after map', e);
-          }
-        }
-      });
     }
-  }
 
-  RED.nodes.registerType("array-map-end", ArrayMapEnd());
-  RED.nodes.registerType("array-map-end-sync", ArrayMapEnd({isSync: true}));
-
-  // function ArrayMapContinue(config) {
-  //   RED.nodes.createNode(this, config);
-  //   const node = this;
-  //
-  //   node.on('input', function (msg) {
-  //     addToResult(msg);
-  //   })
-  // }
-  //
-  // RED.nodes.registerType("array-map-continue", ArrayMapContinue);
+    RED.nodes.registerType("array-map-end", ArrayMapEnd());
+    RED.nodes.registerType("array-map-end-sync", ArrayMapEnd({isSync: true}));
+    RED.nodes.registerType("array-map-break", ArrayBreak());
 };
--- node-red-contrib-common-utils/elastic-logger.js
+++ node-red-contrib-common-utils/elastic-logger.js
@@ -1,89 +1,88 @@
 const {resolvePath, parseTypedInput, getTypedFieldValue} = require('../node-red-contrib-common-utils/1-global-utils');
 
+const log_levels = {
+    debug: {value: 0, name: 'debug', description: ''},
+    notice: {value: 1, name: 'notice', description: ''},
+    info: {value: 2, name: 'info', description: ''},
+    warning: {value: 3, name: 'warning', description: ''},
+    error: {value: 4, name: 'error', description: ''},
+    alert: {value: 5, name: 'alert', description: ''},
+    critical: {value: 6, name: 'critical', description: ''},
+    emergency: {value: 7, name: 'emergency', description: ''},
+}
+
+class UDPClient {
+    constructor(host, port) {
+        this._client = require('dgram').createSocket('udp4');
+        this.host = host;
+        this.port = port;
+    }
+
+    static _encodeData(data) {
+        const encodedDataString = typeof data === 'string'
+            ? data
+            : JSON.stringify(data);
+        return Buffer.from(encodedDataString);
+    }
+
+    send(data, cb = () => {
+    }) {
+        if (cb) {
+            this._client.send(
+                UDPClient._encodeData(data),
+                this.port,
+                this.host,
+                cb
+            )
+        } else {
+            return new Promise((resolve, reject) => {
+                this._client.send(
+                    UDPClient._encodeData(data),
+                    this.port,
+                    this.host,
+                    error => error ? reject(error) : resolve()
+                )
+            });
+        }
+    };
+}
+
+class ElasticLogger {
+    constructor(opts) {
+        this.index_name = opts.index_name;
+        this.lifecycle_token = opts.lifecycle_token;
+        this.parent_lifecycle_token = opts.parent_lifecycle_token;
+        this.udp_client = new UDPClient(opts.host, opts.port);
+    }
+
+    async _logProduction(log_level, message, context, lifecycle_token) {
+
+        try {
+            // log to logstash via udp
+            await this.udp_client.send({
+                _index_name: this.index_name,
+                level: log_level,
+                message,
+                context: context ? JSON.stringify(context) : null,
+                timestamp: (new Date()).toISOString(),
+                lifecycle_token: this.lifecycle_token || lifecycle_token || null,
+                parent_lifecycle_token: this.parent_lifecycle_token || null
+            });
+        } catch (err) {
+            console.error(`Couldn't log [${message}] message`, err, err.meta);
+        }
+    }
+
+    async log(log_level, message, context, lifecycle_token) {
+        return this._logProduction(log_level, message, context, lifecycle_token);
+    }
+}
+
 module.exports = function (RED) {
-    function ElasticLogger(config) {
+    function ElasticLoggerNode(config) {
         RED.nodes.createNode(this, config);
         const node = this;
         this.config = RED.nodes.getNode(config.config);
-
-        const log_levels = {
-            debug: {value: 0, name: 'debug', description: ''},
-            notice: {value: 1, name: 'notice', description: ''},
-            info: {value: 2, name: 'info', description: ''},
-            warning: {value: 3, name: 'warning', description: ''},
-            error: {value: 4, name: 'error', description: ''},
-            alert: {value: 5, name: 'alert', description: ''},
-            critical: {value: 6, name: 'critical', description: ''},
-            emergency: {value: 7, name: 'emergency', description: ''},
-        }
-
-        class UDPClient {
-            constructor(host, port) {
-                this._client = require('dgram').createSocket('udp4');
-                this.host = host;
-                this.port = port;
-            }
-
-            static _encodeData(data) {
-                const encodedDataString = typeof data === 'string'
-                    ? data
-                    : JSON.stringify(data);
-                return Buffer.from(encodedDataString);
-            }
-
-            send(data, cb = () => {
-            }) {
-                if (cb) {
-                    this._client.send(
-                        UDPClient._encodeData(data),
-                        this.port,
-                        this.host,
-                        cb
-                    )
-                } else {
-                    return new Promise((resolve, reject) => {
-                        this._client.send(
-                            UDPClient._encodeData(data),
-                            this.port,
-                            this.host,
-                            error => error ? reject(error) : resolve()
-                        )
-                    });
-                }
-            };
-        }
-
-
-        class ElasticLogger {
-            constructor(opts) {
-                this.index_name = opts.index_name;
-                this.lifecycle_token = opts.lifecycle_token;
-                this.parent_lifecycle_token = opts.parent_lifecycle_token;
-                this.udp_client = new UDPClient(opts.host, opts.port);
-            }
-
-            async _logProduction(log_level, message, context, lifecycle_token) {
-
-                try {
-                    // log to logstash via udp
-                    await this.udp_client.send({
-                        _index_name: this.index_name,
-                        level: log_level,
-                        message,
-                        context: context ? JSON.stringify(context) : null,
-                        timestamp: (new Date()).toISOString(),
-                        lifecycle_token: this.lifecycle_token || lifecycle_token || null,
-                        parent_lifecycle_token: this.parent_lifecycle_token || null
-                    });
-                } catch (err) {
-                    console.error(`Couldn't log [${message}] message`, err, err.meta);
-                }
-            }
-
-            async log(log_level, message, context, lifecycle_token) {
-                return this._logProduction(log_level, message, context, lifecycle_token);
-            }
-        }
 
         const logger = new ElasticLogger({
             index_name: node.config.indexName,
@@ -124,5 +123,5 @@ module.exports = function (RED) {
         });
     }
 
-    RED.nodes.registerType("elastic-logger", ElasticLogger);
+    RED.nodes.registerType("elastic-logger", ElasticLoggerNode);
 }
