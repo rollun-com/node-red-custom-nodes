de9237ace4bc80eb771e725f1e153d1ff7a07677
Maxim DOVHOPOLYI <dovgopolyy0000@gmail.com>
2021-08-01 14:33:39 +0300

    fix: crash if request does not have LT

--- node-red-contrib-common-utils/1-global-utils.js
+++ node-red-contrib-common-utils/1-global-utils.js
@@ -116,7 +116,7 @@ function getLifecycleToken(msg = {}) {
   const { req, __parent_lifecycle_token, __lifecycle_token } = msg;
   console.log('getLifecycleToken', msg);
   return {
-    PLT: __parent_lifecycle_token || req.__parent_lifecycle_token || (req ? req.header('lifecycle_token') || req.header('lifecycletoken') || null : null),
+    PLT: __parent_lifecycle_token || (req && req.__parent_lifecycle_token) || (req ? req.header('lifecycle_token') || req.header('lifecycletoken') || null : null),
     LT: __lifecycle_token || req.__lifecycle_token || randomString(30, 'QWERTYUIOPASDFGHJKLZXCVBNM0123456789'),
   }
 }
